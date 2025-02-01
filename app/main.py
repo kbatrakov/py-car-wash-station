f7adabc895747de66b7e6af06022c1bd8e16b3d3
kbatrakov <kirillbatrakov0819@gmail.com>
2025-02-01 17:33:31 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -16,19 +16,17 @@ class CarWashStation:
 
     def serve_cars(self, car_collection:
                    list) -> float:
-        filtered_cars = [car for car in car_collection
-                         if car.clean_mark < self.clean_power]
-        benefit = round(sum([car.comfort_class
-                        * ((self.clean_power - car.clean_mark)
-                            * self.average_rating)
-                        / self.distance_from_city_center
-                        for car in filtered_cars]), 1)
-        [self.wash_single_car(car) for car in filtered_cars]
-        return benefit
+        benefit = []
+        for car in car_collection:
+            if car.clean_mark < self.clean_power:
+                benefit.append(self.calculate_washing_price(car))
+                self.wash_single_car(car)
+        return round(sum(benefit), 1)
 
-    def calculate_washing_price(self, car: Car) -> None:
-        round((car.comfort_class * (self.clean_power - car.clean_mark)
-               * self.average_rating) / self.distance_from_city_center, 1)
+    def calculate_washing_price(self, car: Car) -> float:
+        return round((car.comfort_class * (self.clean_power - car.clean_mark)
+                      * self.average_rating)
+                     / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
